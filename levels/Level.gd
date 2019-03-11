018488f469cf2b1e47a29acecd9ff6b2e8fd6ee4
Nils ANDRÉ-CHANG <nils.andre.chang@gmail.com>
2019-03-11 20:05:08 +0000

    Make Spatial node root node of each level

--- levels/Level.gd
+++ levels/Level.gd
@@ -1,5 +1,5 @@
 extends Node
 
 func _ready():
-	get_tree().call_group("navigation", "set_navigation", $World/Navigation)
-	get_tree().call_group("player", "set_player", $World/Player)
+	get_tree().call_group("navigation", "set_navigation", $Navigation)
+	get_tree().call_group("player", "set_player", $Player)
